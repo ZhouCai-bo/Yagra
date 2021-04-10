5a4f7412891232828216807cc261dc3782a042c1
ZhouCai-bo <15171506078@163.com>
2021-04-10 22:57:52 +0800

    3.14

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 import MySQLdb
 import json
+import hashlib
 
 app = Flask(__name__)
 app.debug = True
@@ -35,6 +37,8 @@ def login_event():
 		cursor.execute(sql)
 		results = cursor.fetchall()
 		db.close()
+		if len(results) != 0:
+			return render_template("yagra/profile.html")
 		return "got " + str(len(results)) + " results."
 
 @app.route('/register_event', methods = ['GET', 'POST'])
@@ -52,7 +56,7 @@ def register_event():
 		results = cursor.fetchall()
 		
 		if len(results) == 0:
-			sql = "INSERT INTO test_table VALUES('" + username + "','" + str(hash(password)) + "');"
+			sql = "INSERT INTO test_table VALUES('" + username + "','" + str(hashlib.md5(password).hexdigest()) + "');"
 			print(sql)
 			cursor.execute(sql)
 			db.commit()
@@ -61,6 +65,15 @@ def register_event():
 			return render_template("yagra/login.html")
 		else:
 			return render_template("yagra/login.html", existed = True)
+
+@app.route('/upload_event', methods = ['GET', 'POST'])
+def upload_event():
+	if request.method == 'POST':
+		f = request.files['image']
+		path = '/home/ubuntu/sources/demo0/images/' + f.filename
+		f.save(path)
+		return render_template('yagra/profile.html')
+	return render_template('yagra/login.html')
 
 if __name__ == '__main__':
     app.run()
